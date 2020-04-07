ab30c5252b1fff48188ce48d68ae0c2daef10d2f
ningyx-buaa <947208885@qq.com>
2020-04-07 19:55:39 +0800

    file

--- wuhan_2/ringpage/event_new/ring-for-cncert/frontend/src/modules/Home.vue
+++ wuhan_2/ringpage/event_new/ring-for-cncert/frontend/src/modules/Home.vue
@@ -258,6 +258,7 @@ export default {
                       formatter: function (param) {
                          return '{d}%' + param.data.name;
                       }
+                    }
                   },
                   data: [
                       {
